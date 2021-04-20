ca06a6704fd4f9494dc8007c2d371de8a2ae56a4
Mikolaj <mikolaj35@wp.pl>
2021-04-20 23:06:36 +0200

    Menu on top

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Menu } from "./components/menu";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Stronka o informacjach z weselem :)</p>
-        <p>Mapka z dojazdem xd</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Stronka o informacjach z weselem :)</p>
+          <p>Mapka z dojazdem xd</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
